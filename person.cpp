a7258ee979e5728862a14c87380aeb40707e68ae
Nawaraj Shah <shah.nawaraj.ns@gmail.com>
2021-08-03 11:15:03 +0545

    Add files via upload
    added a person file

---
+++ person.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+class Person
+{
+    private:
+        int age;
+        string name, address, email, phoneNo;
+
+    public:
+        //getting person information
+        void getPersonData() {
+            cout<<"Enter age: ";
+            cin>>age;
+            cout<<"Enter phone number: ";
+            cin.get();// very usefull while using getline multiple times
+            getline(cin, phoneNo);
+
+            cout<<"Enter name: ";
+            cin.get();
+            getline(cin, name);
+
+            cout<<"Enter address: ";
+            cin.get();
+            getline(cin, address);
+
+            cout<<"Enter email address: ";
+            cin>>email;
+        };
+
+        // displaying person information
+        void showPersonData(){
+            cout<<"Age: "<<age<<endl;
+            cout<<"Phone number: "<<phoneNo<<endl;
+            cout<<"Name: "<<name<<endl;
+            cout<<"Address: "<<address<<endl;
+            cout<<"Email address: "<<email<<endl;
+        };
+};
+
+// for testing porpose only
+int main()
+{
+    Person person;
+    person.getPersonData();
+    person.showPersonData();
+
+    return 0;
+}
